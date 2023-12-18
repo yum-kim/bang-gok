53916cf7c9e6a13c0aec75b9376cd7a254894c3b
yum-kim <dwkimym93@gmail.com>
2023-12-18 17:58:45 +0900

    style: 전역 폰트 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      regular: ['Pretendard-Regular', 'sans-serif'],
+    },
   },
-  plugins: ['prettier-plugin-tailwindcss'],
+  plugins: ['prettier-plugin-tailwindcss', require('@tailwindcss/forms')],
 };
