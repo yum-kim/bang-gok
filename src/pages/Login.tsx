e9246c0400ea83a52e246c0b4ca5963c9616e84c
yum-kim <dwkimym93@gmail.com>
2023-12-19 13:02:59 +0900

    temp: 임시 로그인 화면 UI 구현

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,20 +10,50 @@ const Login = () => {
 
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <div className="flex flex-col">
+      <div className="flex flex-col p-4 rounded-md">
         <div>
-          <div>logo</div>
-          <h1>Sign in</h1>
+          <div className="mb-5">logo</div>
+          <h1 className="mb-5 text-lg">Sign in</h1>
         </div>
-        <div>
-          <div>
-            <input type="text" />
+        <form className="max-w-sm mx-auto">
+          <div className="mb-5">
+            <input
+              type="email"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="email@email.com"
+              required
+            />
           </div>
-          <div>
-            <input type="text" />
+          <div className="mb-5">
+            <input
+              type="password"
+              id="password"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+            />
           </div>
-          <button>Sign in</button>
-        </div>
+          <div className="flex items-start mb-5">
+            <div className="flex items-center h-5">
+              <input
+                id="remember"
+                type="checkbox"
+                value=""
+                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                required
+              />
+              <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900">
+                Remember me
+              </label>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Login
+          </button>
+        </form>
       </div>
     </div>
   );
